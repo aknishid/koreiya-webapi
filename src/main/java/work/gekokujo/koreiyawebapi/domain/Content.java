82d88fce2b1a2f6128156eecef4b11b6943af9c7
aki <aini@akinori-MacBook-Pro.local>
2018-05-20 17:21:03 +0900

    add domain IknowList.java

--- src/main/java/work/gekokujo/koreiyawebapi/domain/Content.java
+++ src/main/java/work/gekokujo/koreiyawebapi/domain/Content.java
@@ -1,7 +1,31 @@
 package work.gekokujo.koreiyawebapi.domain;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+
 /**
  * Created by aini on 2018/05/20.
  */
+@Entity
+@Data
 public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Size(min = 0,max = 140)
+    private String content;
+
+    private Long userId;
+
+    private Timestamp createBy;
+
+    private String contentImage;
+
+    private Long tagId;
 }
