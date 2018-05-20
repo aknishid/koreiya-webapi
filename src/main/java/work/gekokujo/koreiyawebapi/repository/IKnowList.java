987fdb716d1acdfe017e0766685a125780645d9f
aki <aini@akinori-MacBook-Pro.local>
2018-05-20 20:11:35 +0900

    add domain Tag.java

--- src/main/java/work/gekokujo/koreiyawebapi/repository/IKnowList.java
+++ src/main/java/work/gekokujo/koreiyawebapi/repository/IKnowList.java
@@ -1,10 +1,9 @@
 package work.gekokujo.koreiyawebapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import work.gekokujo.koreiyawebapi.domain.Content;
 
 /**
  * Created by aini on 2018/05/20.
  */
-public interface ContentRepository extends JpaRepository<Content,Long>{
+public interface IKnowList extends JpaRepository<IKnowList,Long> {
 }
--- src/main/java/work/gekokujo/koreiyawebapi/repository/TagService.java
+++ src/main/java/work/gekokujo/koreiyawebapi/repository/TagService.java
@@ -1,10 +1,10 @@
 package work.gekokujo.koreiyawebapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import work.gekokujo.koreiyawebapi.domain.Content;
+import work.gekokujo.koreiyawebapi.domain.Tag;
 
 /**
  * Created by aini on 2018/05/20.
  */
-public interface ContentRepository extends JpaRepository<Content,Long>{
+public interface TagService extends JpaRepository<Tag,Long> {
 }
