549081e96d9b77f8af5beab53933725a99a6edf0
akie <nishida.akie@gmail.com>
2018-05-20 17:23:38 +0900

    edit follow.java

--- src/main/java/work/gekokujo/koreiyawebapi/domain/Follow.java
+++ src/main/java/work/gekokujo/koreiyawebapi/domain/Follow.java
@@ -1,4 +1,20 @@
 package work.gekokujo.koreiyawebapi.domain;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long followId;
+    private long inverseFollowId;
+
 }
+
