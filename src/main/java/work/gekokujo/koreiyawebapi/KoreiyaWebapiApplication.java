fdece63eef80a84bc95242a769027ff142c78fda
Akie Nishida <akie@AkienoMacBook-Pro.local>
2018-05-20 15:55:16 +0900

    add spring initializer

---
+++ src/main/java/work/gekokujo/koreiyawebapi/KoreiyaWebapiApplication.java
@@ -0,0 +1,12 @@
+package work.gekokujo.koreiyawebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreiyaWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreiyaWebapiApplication.class, args);
+	}
+}
