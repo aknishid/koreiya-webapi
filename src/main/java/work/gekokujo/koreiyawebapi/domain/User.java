acaad9a2d16ef274c1fd12b05d4180c5411a6272
akie <nishida.akie@gmail.com>
2018-05-20 17:08:44 +0900

    add file follow.java

--- src/main/java/work/gekokujo/koreiyawebapi/domain/User.java
+++ src/main/java/work/gekokujo/koreiyawebapi/domain/User.java
@@ -1,4 +1,28 @@
 package work.gekokujo.koreiyawebapi.domain;
 
+import com.sun.javafx.beans.IDProperty;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String mail;
+
+    private  String password;
+
+    private String introduction;
+
+    private String userIcon;
+
+
 }
+
+
